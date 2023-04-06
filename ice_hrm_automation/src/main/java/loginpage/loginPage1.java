9a99a271b26fbe23e70bb8a9a63b4c034ba87fd9
Rsoni7790 <rsoni7790@gmail.com>
2023-04-06 15:48:10 +0530

    upload ice_hrm

---
+++ ice_hrm_automation/src/main/java/loginpage/loginPage1.java
@@ -0,0 +1,43 @@
+package loginpage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class loginPage1 {
+	public static LoginPageUsingPOM log;
+	public static WebDriver driver;
+	public static ChromeOptions setChromeOption()
+	{
+		ChromeOptions option=new ChromeOptions();
+		option.addArguments("start-maximized");
+		option.addArguments("--remote-allow-origins=*");
+		option.addArguments("incognito");
+		return option;
+		}
+	   
+	 public static void main(String args[]) throws Throwable
+	 {
+		 
+		 WebDriverManager .chromedriver().setup();
+	  try {	 
+		 driver=new ChromeDriver(setChromeOption());
+		  log=new LoginPageUsingPOM(driver);
+		 driver.get("https://www.saucedemo.com/");
+	       }
+	      catch(Exception e)
+	  {
+		  throw new Exception(e);
+	  }
+	  
+	    log.setUserName("standard_user");
+	    log.setPassword("secret_sauce");
+	     log.setClick();
+	    Thread.sleep(4000);
+	    driver.close();
+	 }
+
+}
